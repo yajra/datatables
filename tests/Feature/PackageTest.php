fcdc47625d9d0caefbc9eaffc511a9d83d058616
Arjay Angeles <aqangeles@gmail.com>
2024-03-14 14:28:52 +0800

    tests: typo

--- tests/Feature/PackageTest.php
+++ tests/Feature/PackageTest.php
@@ -1,6 +1,6 @@
 <?php
 
-test('it loads all package', function () {
+test('it loads all the packages', function () {
     expect(class_exists(\Yajra\DataTables\DataTablesServiceProvider::class))->toBeTrue();
     expect(class_exists(\Yajra\DataTables\HtmlServiceProvider::class))->toBeTrue();
     expect(class_exists(\Yajra\DataTables\FractalServiceProvider::class))->toBeTrue();
