000bb908a9e2dd78b1370535508af354dad25a33
Arjay Angeles <aqangeles@gmail.com>
2025-02-26 20:58:45 +0800

    test: pest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,11 @@
 
 namespace Yajra\DataTables\Installer\Tests;
 
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 use Yajra\DataTables\ButtonsServiceProvider;
 
 abstract class TestCase extends BaseTestCase
 {
-    use LazilyRefreshDatabase;
-
     protected function getPackageProviders($app): array
     {
         return [
